79f9ed4d6265bd4140d963a67b13490f3f8ee783
beatrizlima-tech <bealima.tech@gmail.com>
2026-04-20 13:28:58 -0300

    feat: estrutura inicial do projeto Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,86 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-produtos');
+  
+  private apiUrl = "https://api-produtos-beatriz-java-bvbvd5d8b7a5aha0.canadacentral-01.azurewebsites.net/api/v1/produtos";
+  
+  private http = inject(HttpClient);
+
+  produtos = signal<any[]>([]);
+
+  ngOnInit() {
+    this.http.get(this.apiUrl)
+      .subscribe((data) => {
+        this.produtos.set(data as any[]);
+      })
+  }
+
+  formCadastro = new FormGroup({
+    nome : new FormControl('', [Validators.required]),
+    preco : new FormControl('', [Validators.required]),
+    quantidade : new FormControl('', [Validators.required])
+  });
+
+  formEdicao = new FormGroup({
+    id : new FormControl('', [Validators.required]),
+    nome : new FormControl('', [Validators.required]),
+    preco : new FormControl('', [Validators.required]),
+    quantidade : new FormControl('', [Validators.required]),
+    total : new FormControl('', [Validators.required]),
+  });
+
+  cadastrar() {
+    this.http.post(this.apiUrl, this.formCadastro.value)
+      .subscribe((data: any) => {
+        alert(`Produto "${data.nome}", cadastrado com sucesso!`);
+        this.formCadastro.reset();
+        this.ngOnInit();
+      })
+  }
+
+  obterDados(id: number) {
+    this.http.get(this.apiUrl + "/" + id)
+      .subscribe((data: any) => {
+        this.formEdicao.patchValue({
+          id: data.id,
+          nome: data.nome,
+          preco: data.preco,
+          quantidade: data.quantidade,
+          total: data.total
+        });
+      })
+  }
+
+  atualizar() {
+    this.http.put(this.apiUrl + "/" + this.formEdicao.value.id, this.formEdicao.value)
+      .subscribe((data: any) => {
+        alert(`Produto "${data.nome}", atualizado com sucesso!`);
+        this.formEdicao.reset();
+        this.ngOnInit();
+      })
+  }
+
+  excluir(id: number) {
+    if(confirm('Deseja realmente excluir este produto?')) {
+      this.http.delete(this.apiUrl + "/" + id)
+        .subscribe((data: any) => {
+          alert(`Produto "${data.nome}", excluído com sucesso!`);
+          this.ngOnInit();
+        });
+    }
+  }
+
 }
