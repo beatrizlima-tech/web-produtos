1cc016110d4bbd04461f6e92e2bdc76580a72db1
beatrizlima-tech <bealima.tech@gmail.com>
2026-06-25 14:41:56 -0300

    feat: implementa cadastro e consulta de produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,86 +1,119 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+//Criando uma estrutura de dados para representar um produto
+interface Produto { //Modelo de dados
+   id: number; //Atributo 'id'
+   nome: string; //Atributo 'nome'
+   descricao: string; //Atributo 'descricao'
+   preco: number; //Atributo 'preco'
+   quantidade: number; //Atributo 'quantidade'
+   total: number; //Atributo 'total'
+}
 
 @Component({
   selector: 'app-root',
   imports: [
-    CommonModule,
     FormsModule,
-    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+    //Endereço da API
+    apiUrl: string = 'http://localhost:8081/api/v1/produtos';
+
+    //Criando um objeto da biblioteca HttpClient
+    httpClient = inject(HttpClient);  
+
+    //Variável signal para armazenar a lista de produtos obtida da API
+    produtos = signal<any[]>([]);
+
+    //Variável signal para exibir mensagens e notificações na página
+    mensagem = signal<string>('');
   
-  private apiUrl = "https://api-produtos-beatriz-java-bvbvd5d8b7a5aha0.canadacentral-01.azurewebsites.net/api/v1/produtos";
-  
-  private http = inject(HttpClient);
+    //Atributo para capturar o nome do produto
+    nomeProduto: string = '';
 
-  produtos = signal<any[]>([]);
+    //Atributo para guardar os dados de um produto
+    produtoSelecionado = signal<Produto>({
+      id : 0, //valor padrão
+      nome : '', //valor padrão
+      descricao : '', //valor padrão
+      preco : 0, //valor padrão
+      quantidade : 0, //valor padrão
+      total : 0 //valor padrão
+    });
 
-  ngOnInit() {
-    this.http.get(this.apiUrl)
-      .subscribe((data) => {
-        this.produtos.set(data as any[]);
-      })
-  }
+    //Atributo para guardar uma flag que indique quando o formulário
+    //de cadastro ou edição do produto será exibido
+    exibirFormulario = signal<boolean>(false);
 
-  formCadastro = new FormGroup({
-    nome : new FormControl('', [Validators.required]),
-    preco : new FormControl('', [Validators.required]),
-    quantidade : new FormControl('', [Validators.required])
-  });
+    //Função para ser executada quando o botão de pesquisa for clicado
+    pesquisarProdutos() {
+        //Fazendo uma requisição HTTP GET para consultar os produtos na API
+        this.httpClient
+              .get<any[]>(`${this.apiUrl}/listar?nome=
+                                ${this.nomeProduto}`)
+              .subscribe((data) => { //Capturando a resposta da API
+                  //Armazenar os dados obtidos da API no signal
+                  this.produtos.set(data);
+              });
+    }
 
-  formEdicao = new FormGroup({
-    id : new FormControl('', [Validators.required]),
-    nome : new FormControl('', [Validators.required]),
-    preco : new FormControl('', [Validators.required]),
-    quantidade : new FormControl('', [Validators.required]),
-    total : new FormControl('', [Validators.required]),
-  });
+    //Função para ser executada quando clicarmos no botão de exclusão
+    excluirProduto(id: number) {
+      if(confirm('Deseja realmente excluir o produto selecionado?')) {
+         //Fazendo uma requisição HTTP DELETE para excluir um produto na API
+         this.httpClient
+               .delete(`${this.apiUrl}/excluir/${id}`, 
+                         { responseType: 'text' }) 
+               .subscribe((response) => {
+                  //armazenando a resposta obtida da API
+                  this.mensagem.set(response);
+                  //Fazendo uma nova consulta dos produtos
+                  this.pesquisarProdutos();
+               })
+      }
+    }
 
-  cadastrar() {
-    this.http.post(this.apiUrl, this.formCadastro.value)
-      .subscribe((data: any) => {
-        alert(`Produto "${data.nome}", cadastrado com sucesso!`);
-        this.formCadastro.reset();
-        this.ngOnInit();
-      })
-  }
+    //Função para exibir o formulário de cadastro de produto
+    novoProduto() {
+      this.exibirFormulario.set(true);
+      this.mensagem.set('');
+    }
 
-  obterDados(id: number) {
-    this.http.get(this.apiUrl + "/" + id)
-      .subscribe((data: any) => {
-        this.formEdicao.patchValue({
-          id: data.id,
-          nome: data.nome,
-          preco: data.preco,
-          quantidade: data.quantidade,
-          total: data.total
-        });
-      })
-  }
+    //Função para ocultar o formulário e limpar os valores dos campos
+    cancelarEdicao() {
+      this.exibirFormulario.set(false);
+      this.produtoSelecionado.set({
+        id : 0, //valor padrão
+        nome : '', //valor padrão
+        descricao : '', //valor padrão
+        preco : 0, //valor padrão
+        quantidade : 0, //valor padrão
+        total : 0 //valor padrão
+      });
+    }
 
-  atualizar() {
-    this.http.put(this.apiUrl + "/" + this.formEdicao.value.id, this.formEdicao.value)
-      .subscribe((data: any) => {
-        alert(`Produto "${data.nome}", atualizado com sucesso!`);
-        this.formEdicao.reset();
-        this.ngOnInit();
-      })
-  }
+    //Função para realizar o cadastro do produto na API
+    cadastrarProduto() {
+      //Enviando uma requisição POST para a API
+      this.httpClient.post(`${this.apiUrl}/criar`, 
+              this.produtoSelecionado(), { responseType : 'text' })
+        .subscribe((response) => {
+          this.mensagem.set(response);
+          this.cancelarEdicao();
 
-  excluir(id: number) {
-    if(confirm('Deseja realmente excluir este produto?')) {
-      this.http.delete(this.apiUrl + "/" + id)
-        .subscribe((data: any) => {
-          alert(`Produto "${data.nome}", excluído com sucesso!`);
-          this.ngOnInit();
+          //verificar se há uma consulta de produtos exibida na tela
+          if(this.produtos().length > 0) {
+             this.pesquisarProdutos(); //executando a consulta 
+          }
+
         });
     }
-  }
-
 }
